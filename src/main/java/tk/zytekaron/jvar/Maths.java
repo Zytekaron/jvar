4de225a3629b76397e97c5f603a93900c84ddd32
Michael Thornes <zytekaron@gmail.com>
2020-02-06 07:15:31 -0800

    Updates

--- src/main/java/tk/zytekaron/jvar/Maths.java
+++ src/main/java/tk/zytekaron/jvar/Maths.java
@@ -1,5 +1,15 @@
 package tk.zytekaron.jvar;
 
 public class Maths {
+    public static int constrain(int num, int min, int max) {
+        return Math.min(Math.max(num, min), max);
+    }
     
+    public static long constrain(long num, long min, long max) {
+        return Math.min(Math.max(num, min), max);
+    }
+    
+    public static double constrain(double num, double min, double max) {
+        return Math.min(Math.max(num, min), max);
+    }
 }
